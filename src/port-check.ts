0211ab6bb4aa8f382e2db127c561f57d214f5dc4
Monve <chenmenghui@lazbao.com>
2022-02-23 16:50:19 +0800

    fix: node:net not found

--- src/port-check.ts
+++ src/port-check.ts
@@ -2,16 +2,16 @@
  * @Author: Monve
  * @Date: 2021-11-25 18:09:03
  * @LastEditors: Monve
- * @LastEditTime: 2022-02-23 16:15:46
+ * @LastEditTime: 2022-02-23 16:48:53
  * @FilePath: /port-check/src/port-check.ts
  */
 
-import { Socket } from 'node:net';
+import { Socket } from 'net';
 
 interface HOST_INFO {
   host: string,
   port: number,
-  timeout: number
+  timeout?: number
 }
 
 export const isReachable = async ({ host = '127.0.0.1', port, timeout = 1000 }: HOST_INFO): Promise<boolean> => {
